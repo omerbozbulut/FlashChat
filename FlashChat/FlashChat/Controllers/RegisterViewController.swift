0591b9e2524bb8c26c33534b0d7820bde4ebd223
omer <omerbzblt613@gmail.com>
2022-04-29 23:12:16 +0300

    user create in Firebase

--- FlashChat/FlashChat/Controllers/RegisterViewController.swift
+++ FlashChat/FlashChat/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +15,19 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text{
+            
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let err = error{
+                    print(err)
+                }else{
+                    // başarıyla kayıt olduysa
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+          
+        }
     }
     
+}
 }
