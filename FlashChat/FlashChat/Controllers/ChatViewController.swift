4f66f50d8c673f0f23752b00032f161c9b0acdfa
omer <omerbzblt613@gmail.com>
2022-05-10 16:33:57 +0300

    Messages save the Firestore

--- FlashChat/FlashChat/Controllers/ChatViewController.swift
+++ FlashChat/FlashChat/Controllers/ChatViewController.swift
@@ -9,16 +9,15 @@
 import UIKit
 import Firebase
 
+
 class ChatViewController: UIViewController{
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
-    var messages : [Message] = [
-    Message(sender: "a@s.com", body: "Hi"),
-    Message(sender: "a@s.com", body: "Whats up broo"),
-    Message(sender: "a@s.com", body: "Whats up broo asdasdasd  asdasdasd asdasdasd asdasdasd asdasdasdasdasdasdasdasdasd asdasdasdasdasdasd asdasdasd sadasd")
-    ]
+    let db = Firestore.firestore()
+    
+    var messages : [Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,9 +26,53 @@ class ChatViewController: UIViewController{
         navigationItem.hidesBackButton = true
         
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        loadMessages()
+    }
+    
+    func loadMessages(){
+        
+        db.collection(K.FStore.collectionName).addSnapshotListener{ (querySnapshot, err) in
+            self.messages = []
+            if let e = err {
+                print("Error getting documents: \(e)")
+            } else {
+                if let snapshotDoc = querySnapshot?.documents{
+                    for doc in snapshotDoc{
+                        let data = doc.data()
+                        if let body = data[K.FStore.bodyField] as? String,
+                           let sender = data[K.FStore.senderField] as? String{
+                            let message = Message(sender: sender, body: body)
+                            self.messages.append(message)
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextfield.text,
+           let messageSenderEmail = Auth.auth().currentUser?.email{
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.bodyField: messageBody,
+                K.FStore.senderField: messageSenderEmail
+            ]) { error in
+                if let error = error {
+                    print("Error adding document: \(error)")
+                }else{
+                    print("Successfully saved data")
+                }
+            }
+
+        }
+         
+        
     }
     
 
@@ -52,6 +95,7 @@ extension ChatViewController : UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        
         cell.label.text = messages[indexPath.row].body
         return cell
     }
